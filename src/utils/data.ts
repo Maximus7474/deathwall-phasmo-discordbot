21da21cdffc73ffc14c3f9fa0c1b83d4329d6a36
Maximus7474 <the.maximus.7474@gmail.com>
2026-01-02 23:04:02 +0100

    feat(utils): data file with ghost names

---
+++ src/utils/data.ts
@@ -0,0 +1,6 @@
+export const GHOST_TYPES = [
+    'Banshee', 'Dayan', 'Demon', 'Deogen', 'Gallu', 'Goryo', 'Hantu', 'Jinn',
+    'Mare', 'Moroi', 'Myling', 'Obake', 'Obambo', 'Oni', 'Onryo', 'Phantom',
+    'Poltergeist', 'Raiju', 'Revenant', 'Shade', 'Spirit', 'Thaye', 'The Mimic',
+    'The Twins', 'Wraith', 'Yokai', 'Yurei'
+];
