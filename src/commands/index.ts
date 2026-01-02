239ab72312f4e0a4efe2e33110761f34aacf4f68
Maximus7474 <the.maximus.7474@gmail.com>
2026-01-02 22:36:09 +0100

    chore: removed unused example features

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,10 +1,8 @@
 import SlashCommand from "../classes/slash_command";
-import favorite_food from "./favorite_food";
 import help from "./help";
 import ping from "./ping";
 
 export default [
-    favorite_food,
     ping,
     help,
 ] as SlashCommand[];
