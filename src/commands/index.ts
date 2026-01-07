4877345081365e181166d1957655498605acd3d0
Maximus7474 <the.maximus.7474@gmail.com>
2026-01-07 19:05:56 +0100

    chore(commands): add session command to bot directory

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,8 +1,10 @@
 import SlashCommand from "../classes/slash_command";
 import help from "./help";
 import ping from "./ping";
+import session from "./session";
 
 export default [
     ping,
     help,
+    session,
 ] as SlashCommand[];
