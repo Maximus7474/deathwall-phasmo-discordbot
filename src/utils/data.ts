cefe31749d86489dc7961c91b5dd0ccce9a41648
Maximus7474 <the.maximus.7474@gmail.com>
2026-01-17 18:38:37 +0100

    tweak(utils/data): as const for better type inference

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -2,11 +2,11 @@ export const GHOST_TYPES = [
     "banshee", "dayan", "demon", "deogen", "gallu", "goryo", "hantu", "jinn", "mare", "moroi", "myling",
     "obake", "obambo", "oni", "onryo", "phantom", "poltergeist", "raiju", "revenant", "shade", "spirit",
     "thaye", "themimic", "thetwins", "wraith", "yokai", "yurei"
-];
+] as const;
 
 export const ITEMS = [
     "emf_reader", "spirit_box", "thermometer", "dots_projector", "ghost_writing_book", "video_camera",
     "uv_light", "flashlight", "strong_flashlight", "crucifix", "smudge_sticks", "sanity_medication",
     "motion_sensor", "sound_sensor", "salt", "parabolic_microphone", "photo_camera", "firelight",
     "igniter", "tripod", "head_gear"
-];
+] as const;
