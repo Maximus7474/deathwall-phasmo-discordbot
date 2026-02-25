4c0b4e213e72bf7f6775db14d62afc12028715a6
Maximus7474 <the.maximus.7474@gmail.com>
2026-02-25 20:40:40 +0100

    fix(data): item list

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -6,7 +6,7 @@ export const GHOST_TYPES = [
 
 export const GAME_ITEMS = [
     "emf_reader", "spirit_box", "thermometer", "dots_projector", "ghost_writing_book", "video_camera",
-    "uv_light", "flashlight", "strong_flashlight", "crucifix", "smudge_sticks", "sanity_medication",
-    "motion_sensor", "sound_sensor", "salt", "parabolic_microphone", "photo_camera", "firelight",
+    "uv_light", "flashlight", "crucifix", "incense", "sanity_medication",
+    "motion_sensor", "sound_sensor", "sound_recorder", "salt", "parabolic_microphone", "photo_camera", "firelight",
     "igniter", "tripod", "head_gear"
 ] as const;
